8619ad9f0fde6ce0a405fe2d836d1db949b302a1
rogerogers <40619032+rogerogers@users.noreply.github.com>
2020-12-22 11:23:53 +0800

    add clone

--- ln.sh
+++ ln.sh
@@ -1,3 +1,7 @@
-#!/usr/bin/bash
+#!/usr/bin/env sh
+set -ex
+
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 ln -s $(pwd)/my_configs.vim $HOME/.vim_runtime/my_configs.vim
