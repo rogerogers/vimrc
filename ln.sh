dccfb6df60993dfcc7ee135d3eac71e14fc05def
rogerogers <40619032+rogerogers@users.noreply.github.com>
2020-12-22 11:24:45 +0800

    add vim plug

--- ln.sh
+++ ln.sh
@@ -5,3 +5,6 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 ln -s $(pwd)/my_configs.vim $HOME/.vim_runtime/my_configs.vim
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
