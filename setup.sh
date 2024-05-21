81046bfb0f1db743a75abd346502f67df3730862
rogerogers <rogers@rogerogers.com>
2024-05-21 22:03:12 +0800

    chore: git
    Signed-off-by: rogerogers <rogers@rogerogers.com>

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 set -ex
 
 # setup nvm and node begin
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-#source ~/.zshrc
+source ~/.zshrc
 
-#nvm install --lts
-#npm install -g pnpm yarn
+nvm install --lts
+npm install -g pnpm yarn
 # setup nvm and node end
 
 # setup python deps begin
@@ -15,6 +15,11 @@ set -ex
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 # pip install --user ruff poetry pipenv
 # setup python deps end
+
+# setup git
+git config --global core.excludesfile ~/.gitignore
+cp .gitignore ~/.gitignore
+# setup git
 
 # setup golang begin
 go install golang.org/x/tools/cmd/goimports@latest
