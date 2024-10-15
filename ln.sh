c5c890435f5b2a39b8a411962205e522b77a4a3e
rogerogers <rogers@rogerogers.com>
2024-10-15 09:43:24 +0800

    chore: update ln.sh
    Signed-off-by: rogerogers <rogers@rogerogers.com>

--- ln.sh
+++ ln.sh
@@ -4,7 +4,12 @@ set -ex
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
+# vimrc
 ln -s $(pwd)/my_configs.vim $HOME/.vim_runtime/my_configs.vim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# global gitignore
+git config --global core.excludesFile '~/.gitignore'
+ln -snf $(pwd)/.gitignore ~/.gitignore
