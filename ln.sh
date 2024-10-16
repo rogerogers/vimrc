9c29451312b4cbc92a8a0184f95b2fe51c007830
rogerogers <rogers@rogerogers.com>
2024-10-16 15:44:16 +0800

    chore: check dir
    Signed-off-by: rogerogers <rogers@rogerogers.com>

--- ln.sh
+++ ln.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 set -ex
 
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+if [ ! -d ~/.vim_runtime ]; then
+	git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+	sh ~/.vim_runtime/install_awesome_vimrc.sh
+	echo "Installed vimrc"
+fi
 
 # vimrc
 ln -s $(pwd)/my_configs.vim $HOME/.vim_runtime/my_configs.vim
