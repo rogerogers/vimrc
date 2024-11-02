f737baca740948eab22b9463813d0bfd906693d7
rogerogers <rogers@rogerogers.com>
2024-11-02 17:35:26 +0800

    feat: add proxy.sh
    Signed-off-by: rogerogers <rogers@rogerogers.com>

---
+++ proxy.sh
@@ -0,0 +1,16 @@
+# go
+go env -w GOPROXY=https://goproxy.cn,direct
+
+# rust
+cat << EOF > ~/.cargo/config.toml
+[source.crates-io]
+replace-with = 'rsproxy-sparse'
+[source.rsproxy]
+registry = "https://rsproxy.cn/crates.io-index"
+[source.rsproxy-sparse]
+registry = "sparse+https://rsproxy.cn/index/"
+[registries.rsproxy]
+index = "https://rsproxy.cn/crates.io-index"
+[net]
+git-fetch-with-cli = true
+EOF
